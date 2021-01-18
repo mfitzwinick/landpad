beb4b228390984a23c71fa661f887a8e1c3acd4a
Yessica Samuels <yessicaperez@me.com>
2021-01-18 12:39:07 -0800

    new

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,14 +1,75 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Post from "../pages/VisionBoard";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
 
 function LogIn() {
     return (
-
-        <div>
-
-        </div>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
     )
 
 }
 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+// @media all and (min-width: 480px) {
+//   .Login {
+//     padding: 60px 0;
+//   }
+
+//   .Login form {
+//     margin: 0 auto;
+//     max-width: 320px;
+//   }
+// }
+
+<Route exact path="/login">
+  <Login />
+</Route>
+
+import Login from "./containers/Login";
 
 export default LogIn;
