c9d6a5d0be7a75ec4e6d025a91d44b59f1ea702a
jesscru <jessicacrump7@gmail.com>
2021-01-19 19:48:17 -0700

    organized and styled many things

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
+import './styling/App.css';
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Login from './pages/LogIn';
+import Login from './pages/LogIn';
 import VisionBoard from "./pages/VisionBoard";
 import Financial from "./pages/Financial";
-// import Posts from "./pages/Posts";
+import Posts from "./pages/Posts";
 import Data from "./pages/Data";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import Modal from "./components/Modal";
-import UserPost from './components/UserPost';
+import Modal from "./pages/Modal";
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <UserPost />
       <Router>
-        <Route exact path="/" component={Modal} />
-        {/* <Route exact path="/posts" component={Posts} /> */}
-        <Route exact path="/financial" component={Financial} />
-        <Route exact path="/vision-board" component={VisionBoard} />
-        <Route exact path="/data" component={Data} />
-        <Route exact path="/modal" component={Modal} />
+         <div className="page-container">
+          <Navigation />
+        
+          <Route exact path="/" component={Login} />
+          <Route exact path="/modal" component={Modal} />
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/finances" component={Financial} />
+          <Route exact path="/vision-board" component={VisionBoard} />
+          <Route exact path="/learn" component={Data} />
+        
+          <Footer />
+        </div>
+
       </Router>
-      <Footer />
-    </div>
+   
   );
 }
 
