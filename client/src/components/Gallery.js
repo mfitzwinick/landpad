c44afcb65d4183af2b46e46587b1e5aea61ad040
Dibello80 <angelodibellomusic@gmail.com>
2021-01-22 10:53:53 -0800

    Gallery/Team

---
+++ client/src/components/Gallery.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function Gallery (){
+    return (
+        <div>
+            <div className="col-md-4">
+    <div className="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img src="/w3images/lights.jpg" alt="Lights" style="width:100%" />
+        <div className="caption">
+          <p>Michelle</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div className="col-md-4">
+    <div className="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img src="/w3images/lights.jpg" alt="Lights" style="width:100%" />
+        <div className="caption">
+          <p>Yess</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div className="col-md-4">
+    <div className="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img src="/w3images/lights.jpg" alt="Lights" style="width:100%" />
+        <div className="caption">
+          <p>Jessica</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div className="col-md-4">
+    <div className="thumbnail">
+      <a href="/w3images/lights.jpg">
+        <img src="/w3images/lights.jpg" alt="Lights" style="width:100%" />
+        <div className="caption">
+          <p>Angelo</p>
+        </div>
+      </a>
+    </div>
+  </div>
+        </div>
+    )
+    
+}
+    
+        
+
+export default Gallery;
--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import Gallery from "../components/Gallery";
 
 function Team () {
     return (
         <div>
             <div>
             <Navigation />
-                
+            <Gallery/>    
             <Footer />
         </div>
 
