8c415a13c29483ca99f4111f980131a2452f001f
Dibello80 <angelodibellomusic@gmail.com>
2021-01-22 10:28:52 -0800

    team

---
+++ client/src/pages/Team.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+function Team () {
+    return (
+        <div>
+            <div>
+            <Navigation />
+                
+            <Footer />
+        </div>
+
+        </div>
+    )
+}
+
+export default Team
